1638423a022c59c35409507ae4475a49ed7cc03a
Dima Matroskin <dima.matroskin062@gmail.com>
2018-12-20 17:21:18 +0200

    initial commit

---
+++ MMOKPI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+
+namespace MMOKPI
+{
+    class Program
+    {
+        static ITelegramBotClient botClient = new TelegramBotClient("687056656:AAHq_gtma0pgLCQmDMHcgNhvnC3ZgeZdxn4");
+
+        static void Main(string[] args)
+        {
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine($"Hello, World! I am bot {me.Id} and my name is {me.FirstName}");
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            Message msg = e.Message;
+
+            if (msg != null)
+            {
+                Console.WriteLine($"Received a message {msg.Text} in chat with {msg.Chat.Username}");
+
+                await botClient.SendTextMessageAsync(
+                    chatId: msg.Chat.Id,
+                    text: $"Hello {msg.From.FirstName}!");
+            }
+        }
+    }
+}
